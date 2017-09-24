cae0f7be9f0d8c1e9d3986f715c8d94c74668d68
Miguel Figueroa <miguelfigueroa90@gmail.com>
2017-09-24 15:57:12 -0430

    integrando archivos del layout en default.blade.php para el layout principal

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,4 +1,41 @@
 @extends('layouts.default')
 @section('content')
-    i am the home page
-@stop
+<div class="art-layout-cell art-sidebar1">
+    <div class="art-block clearfix">
+        <div class="art-blockcontent">
+            {!! Form::open(['url' => 'login', 'class' => 'form']) !!}
+            <div class="form-group">
+                {!! Form::label('usuario') !!}
+                {!! Form::text('usuario', null, array('required', 'class'=>'form-control', )) !!}
+            </div>
+            <div class="form-group">
+                {!! Form::label('clave') !!}
+                {!! Form::password('clave', null, array('required', 'class'=>'form-control', )) !!}
+            </div>
+            <br>
+            <div class="form-group">
+                {!! Form::submit('ingresar', 
+                array('class'=>'art-button')) !!}
+            </div>
+            {!! Form::close() !!}
+        </div>
+    </div>
+</div>
+<div class="art-layout-cell art-content">
+    <article class="art-post art-article">
+        <div class="art-postcontent art-postcontent-0 clearfix">
+            <div class="art-content-layout">
+                <div class="art-content-layout-row">
+                    <div class="art-layout-cell layout-item-2" style="" >
+                        <h3 style="">Sistema de Reclamos</h3><div class="clearfix">
+                            <div class="image-caption-wrapper" style="">
+                                <img src="images/logo_bav.JPG.png" style="" alt="an image" class="art-lightbox">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </article>
+</div>
+@endsection
